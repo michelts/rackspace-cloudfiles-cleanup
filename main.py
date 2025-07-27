a1ac6a7fdf72f1a6db3d436c12fbe8b2d27ccfef
Michel Sabchuk <michel@turbosys.com.br>
2025-07-27 13:50:46 -0300

    Better organize the code

--- main.py
+++ main.py
@@ -3,35 +3,39 @@ import pyrax
 import argparse
 
 
+def get_cloudfiles_sdk(args):
+    pyrax.set_setting("identity_type", "rackspace")
+    pyrax.set_credentials(args.username, args.api_key, region=args.region)
+    return pyrax.cloudfiles
+
+
 class Parser:
-    def __init__(self, args):
+    def __init__(self, cloudfiles_sdk, args):
+        self.cloudfiles_sdk = cloudfiles_sdk
         self.args = args
-        if not args.list_containers:
-            self.container = self.get_cloudfiles_container()
 
     def cleanup(self):
         print("Starting cleanup")
-        self.delete_objects(self.args.folder_prefix)
-        self.container.delete()
+        container = self.get_cloudfiles_container()
+        self.delete_objects(container, self.args.folder_prefix)
+        container.delete()
         print("Cleanup complete")
 
     def get_cloudfiles_container(self):
-        pyrax.set_setting("identity_type", "rackspace")
-        pyrax.set_credentials(
-            self.args.username, self.args.api_key, region=self.args.region
-        )
-        return pyrax.cloudfiles.get_container(self.args.container_name)
+        return self.cloudfiles_sdk.get_container(self.args.container_name)
 
-    def delete_objects(self, prefix):
-        objs = self.container.get_objects(prefix=prefix)
+    def delete_objects(self, container, prefix):
+        objs = container.get_objects(prefix=prefix)
         for obj in objs:
-            self.container.delete_object(obj.name)
+            container.delete_object(obj.name)
             print(f"- deleted: {obj.name}")
 
-    def list_containers(self):
-        containers = pyrax.cloudfiles.list()
-        for container in containers:
-            print(container.name)
+
+def list_containers(cloudfiles_sdk):
+    containers = cloudfiles_sdk.list()
+    print("Available containers:")
+    for container in containers:
+        print(container.name)
 
 
 def parse_arguments():
@@ -41,13 +45,13 @@ def parse_arguments():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     required = parser.add_argument_group("Required arguments")
-    optional = parser.add_argument_group("Optional arguments")
-    optional.add_argument(
+    required.add_argument(
         "--username", required=True, help="Rackspace cloud username", metavar="USERNAME"
     )
-    optional.add_argument(
+    required.add_argument(
         "--api-key", required=True, help="Rackspace API key", metavar="KEY"
     )
+    optional = parser.add_argument_group("Optional arguments")
     optional.add_argument(
         "--region", default="DFW", help="Rackspace region", metavar="REGION"
     )
@@ -63,17 +67,16 @@ def parse_arguments():
         metavar="NAME",
     )
     optional.add_argument(
-        "--list-containers", action='store_true', help="List all containers"
+        "--list-containers", action="store_true", help="List all containers"
     )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
+    cloudfiles_sdk = get_cloudfiles_sdk(args)
     if args.list_containers:
-        parser = Parser(args)
-        parser.list_containers()
+        list_containers(cloudfiles_sdk)
     else:
-        parser = Parser(args)
+        parser = Parser(cloudfiles_sdk, args)
         parser.cleanup()
-        print("Container deleted.")
