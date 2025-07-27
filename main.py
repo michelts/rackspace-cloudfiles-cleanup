2115f8ad5fec78abe7c3c80f8c1e9de84aef48c4
Michel Sabchuk <michel@turbosys.com.br>
2025-07-27 12:12:30 -0300

    style: improve argparse argument formatting

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ USERNAME = "your_rackspace_username"
 API_KEY = "your_rackspace_api_key"
 REGION = "DFW"
 
+
 def parse_arguments():
-    """Parse command-line arguments."""
-    parser = argparse.ArgumentParser(description='Clean up Rackspace Cloud Files container')
-    parser.add_argument('--container-name', required=True, help='Name of the container to clean up')
-    parser.add_argument('--folder-prefix', default='', help='Folder path prefix to clean up (default: root)')
+    parser = argparse.ArgumentParser(
+        description="Clean up Rackspace Cloud Files container"
+    )
+    parser.add_argument(
+        "--container-name", required=True, help="Name of the container to clean up"
+    )
+    parser.add_argument(
+        "--folder-prefix",
+        default="",
+        help="Folder path prefix to clean up (default: root)",
+    )
     return parser.parse_args()
+
 
 args = parse_arguments()
 
